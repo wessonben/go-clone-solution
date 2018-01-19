aa07505369f7a07975b26ba3d58571f5d1d2fa8b
Ben Wesson <ben.wesson@outlook.com>
2018-01-19 15:03:29 +0000

    Updated to capture start directory from user input.

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
 func main() {
 
+	directory := getDirectory()
+
+	fmt.Println(directory)
+}
+
+func getDirectory() string {
+	directory := ""
+
+	fmt.Print("Enter directory or leave blank for current: ")
+	fmt.Scanln(&directory)
+
+	if directory == "" {
+		ex, _ := os.Executable()
+		directory = path.Dir(ex)
+	}
+
+	return directory
 }
